846cc38b14fafcdc6e7cc01be04376816d1192da
Angelo <angelo.sedler@gmail.com>
2020-08-11 13:38:05 -0300

    S4 & S5, y sus relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+// const Category = require('./models/Category');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: true, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 
@@ -30,10 +31,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product } = sequelize.models;
+const { Product, Category } = sequelize.models; // <--Angelo Modifico
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+ Product.hasMany(Category);// <--Angelo Modifico
+ Category.hasMany(Product);// <--Angelo Modifico
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
