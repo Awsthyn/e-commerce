961eba2f607f4b27c9317969f9c4a50287342be0
Angelo <angelo.sedler@gmail.com>
2020-08-12 13:49:10 -0300

    small bug

--- api/src/db.js
+++ api/src/db.js
@@ -42,8 +42,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Product, Category } = sequelize.models; // <--Angelo Modifico
 
 // Aca vendrian las relaciones
- Product.belongsToMany(Category);// <--Angelo Modifico
- Category.belongsToMany(Product);// <--Angelo Modifico
+ Product.belongsToMany(Category, {through: 'productsInCategory'});// <--Angelo Modifico
+ Category.belongsToMany(Product, {through: 'categoriesOfProduct'});// <--Angelo Modifico
 
  //Nota de Angelo: me tira un warning de que tenemos una promesa sin error handler: 
  // UnhandledPromiseRejectionWarning: Error: Cyclic dependency found. categories is dependent of itself.
