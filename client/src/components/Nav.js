d0ad6a0fb0160222afd47679f1afea9e41bcb71d
Angelo <angelo.sedler@gmail.com>
2020-08-14 11:24:40 -0300

    bugs posta

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from './searchBar.jsx';
+import SearchBar from './SearchBar.jsx';
 
 
 function Nav({onSearch}) {
