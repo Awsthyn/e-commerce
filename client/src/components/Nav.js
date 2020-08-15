9ce46969e2d765d3b0c5bbf15eaa5e65452e817b
Elena Note <elebeatrizgonzalez@gmail.com>
2020-08-15 17:30:06 -0300

    Nuevo estilo de la barra de navegación, con el link para agregar una categoría

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar.jsx';
+import React, { useState } from 'react';
+import { Link, NavLink} from 'react-router-dom';
+import SearchBar from './searchBar.jsx';
 
 
 function Nav({onSearch}) {
+  let [clase, setClase] = useState("btn btn-success");
+
+ function handleClick(e) {
+     setClase = {
+         clase: "btn btn-success active"
+     }
+    console.log('The link was clicked.');
+  }
   return (
-    <nav className="navbar navbar-dark bg-dark ">
-      <div class="btn-group" role="group" aria-label="Basic example">
-        <Link to= "/catalog">
-          <button className="btn btn-sm btn-secondary">Catalogo</button>
-        </Link>
-        <Link to="/products/:id">
-          <button className="btn btn-sm btn-secondary">Detalle Producto</button>
-        </Link>
-        <SearchBar
-          onSearch={onSearch}
-        />
-        <Link to="/products/form/new">
-          <button className="btn btn-sm btn-success">New Product</button>
-        </Link>
-        <Link to="/products/1/edit">
-          <button className="btn btn-sm btn-primary">Edit Form---link hardcodeado</button>
-        </Link>
-        <Link to="/categories/form/new">
-          <button className="btn btn-sm btn-success">New Category </button>
-        </Link>
-      </div>
-    </nav>
+   <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+          <div className="navbar-nav" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li classNameName="nav-item">
+                <NavLink to= "/catalog" className="nav-link">Catalogo</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/products/:id" className="nav-link">Detalle Producto</NavLink>
+              </li>
+            <li className="nav-item">
+               <NavLink to="/products/form/new" className="nav-link">Agregar Producto</NavLink>
+             </li>
+             <li className="nav-item">
+                <NavLink to="/products/1/edit" className="nav-link">Editar Producto</NavLink>
+              </li>
+            <li className="nav-item">
+               <NavLink to="/categories/form/new" className="nav-link">Agregar Categoría</NavLink>
+             </li>
+           </ul>
+            <SearchBar
+              onSearch={onSearch}
+            />
+            </div>
+      </nav>
+    </div>
   );
 };
 
