c7662185938767aa21d466b57f1745f7ff3b8425
nico-slk <nico1991.6@gmail.com>
2020-08-15 18:16:45 -0300

    's12'

--- client/src/components/catalogo.jsx
+++ client/src/components/catalogo.jsx
@@ -1,27 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard";
-import S from"../css/catalogo.module.css";
+import S from "../css/catalogo.module.css";
 
-
-
-const catalogo = ({array, categories}) => {
-  console.log(categories);
-  return (
-    <div className="container row" >
-      <div>
-        <h1 className= {S.title}>Categorias</h1>
-        <ul>{categories.map((e) => (
-        <li key={e} id={e.id} name= {e.name} >{e.name}</li>
-      ))}
-        </ul>
-      </div>
-      <div className="row">
-      {array.map((e) => (
-        <ProductCard key={e} name= {e.name} price={e.price} image= {e.image} />
-      ))}
-      </div>
-    </div>
-  );
+const catalogo = ({ array, categories, filter }) => {
+    console.log(categories);
+    return (
+        <div className="container row">
+            <div>
+                <h1 className={S.title}>Categorias</h1>
+                <ul>
+                    {categories.map((e) => (
+                        <Link>
+                            <li
+                                key={e}
+                                id={e.id}
+                                name={e.name}
+                                onClick={(e) =>
+                                    filter(e.target.getAttribute("name"))
+                                }
+                            >
+                                {e.name}
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className="row">
+                {array.map((e) => (
+                    <ProductCard
+                        key={e}
+                        name={e.name}
+                        price={e.price}
+                        image={e.image}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default catalogo;
