c2060e5cb870e787c1c4a108501f97f27320d741
Awsthyn <aguswagner008@gmail.com>
2020-08-17 20:31:34 -0300

    Todo funcionando

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -65,16 +65,7 @@ function Nav({ onSearch, categories, filter, toGetProducts }) {
               </NavLink>
             </li>
             <li className="nav-item">
-
-              <NavLink to="/categories/form/new" className="nav-link">
-                Agregar Categoría
-              </NavLink>
-            </li>
-          </ul>
-          <SearchBar onSearch={onSearch} />
-        </div>
-
-               <NavLink to="/CrudCategory" className="nav-link">Administrar Categorías</NavLink>
+            <NavLink to="/CrudCategory" className="nav-link">Administrar Categorías</NavLink>
              </li>
            </ul>
             <SearchBar
@@ -85,6 +76,8 @@ function Nav({ onSearch, categories, filter, toGetProducts }) {
       </nav>
     </div>
   );
+
+
 }
 
 export default Nav;
