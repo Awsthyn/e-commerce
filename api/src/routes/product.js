394125d68cf38b41f661b2a1f0d0c18180c2abdc
Elena B. Gonzalez <elebeatrizgonzalez@gmail.com>
2020-08-18 10:40:23 -0300

    Ruta post del producto ahora guarda las categorías asociadas al mismo

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -39,10 +39,33 @@ server.post("/", (req, res,next) =>{
 })
 
 
-server.put("/:id", (req, res, next) => {
+/*server.put("/:id", (req, res, next) => {
   try {
     const { id } = req.params;
-    const { name, description, price, stock, image } = req.body;
+    const { name, description, price, stock, image, category } = req.body;
+    const categories = category;
+    Product.findByPk(id , { include: [Category] } )
+    .then(product => {
+
+        product.removeCategories(product.categories)
+        .then( () => {
+            console.log('catef', categories)
+
+            categories.forEach(category => {
+                if(category.id){
+                    Category.findByPk(category.id)
+                    .then(c => product.addCategory(c))
+                }else{
+                    Category.findByPk(category)
+                    .then(c => product.addCategory(c))
+
+                }
+            })
+
+            res.sendStatus(200)
+          })
+
+        }).catch(next);
     Product.update(
       {
         name,
@@ -51,12 +74,36 @@ server.put("/:id", (req, res, next) => {
         stock,
       },
       { where: { id } }
-    ).then(() => {
+  ).then((product) => {
+      console.info("Producto actualizado ", product)
       res.sendStatus(200);
-    });
+  });
   } catch (error) {
     console.error(error.message);
   }
+}*/
+
+    server.put("/:id", (req, res, next) => {
+    const { id } = req.params;
+    const { name, description, price, stock, image ,category } = req.body;
+    Product.findAll({ include: [Category, Image] })
+      .then((product) => {
+          product.map(i => setCategory(i.dataValues.name))
+        Product.update(
+          {
+            name,
+            description,
+            price,
+            stock,
+            category
+          },
+          { where: { id } }
+        ).then(() => {
+          res.sendStatus(200);
+        });
+    }).catch(next)
+    console.error("Error al actualizar los datos")
+
 });
 
 server.delete("/:id", (req, res, next) => {
