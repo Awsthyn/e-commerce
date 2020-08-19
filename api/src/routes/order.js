b7ae0bb3b57d4c76a4e854978ed20c6f68f3a57c
Awsthyn <aguswagner008@gmail.com>
2020-08-19 19:42:56 -0300

    correccion en routes/order.js

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,73 +1,6 @@
 const server = require("express").Router();
-const { Order } = require("../db.js");
+const { Order, OrderLine } = require("../db.js");
 
-server.post("/user/:idUser/cart", (req, res, next) => {
-  const {
-    userName,
-    shippingAdress,
-    billingAdress,
-    creationDate,
-    total,
-  } = req.body;
-  Order.create({
-    userName,
-    shippingAdress,
-    billingAdress,
-    creationDate,
-    total,
-  })
-    .then(() => {
-      res.sendStatus(201);
-    })
-    .catch(next);
-});
-
-server.get("/user/:idUser/cart", (req, res, next) => {
-  Order.findAll()
-    .then((orders) => {
-      // console.log(products);
-      res.json(orders);
-    })
-    .catch(next);
-});
-
-server.delete("/users/:idUser/cart/", (req, res, next) => {
-  try {
-    const { id } = req.params;
-    Order.destroyAll({ where: { id: id } }).then(() => {
-      res.sendStatus(200);
-    });
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-server.put("/users/:idUser/cart/", (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const {
-      userName,
-      shippingAdress,
-      billingAdress,
-      creationDate,
-      total,
-    } = req.body;
-    Order.update(
-      {
-        userName,
-        shippingAdress,
-        billingAdress,
-        creationDate,
-        total,
-      },
-      { where: { id } }
-    ).then(() => {
-      res.sendStatus(200);
-    });
-  } catch (error) {
-    console.error(error.message);
-  }
-});
 
 //A partir de Aca agreo Ariel
 
@@ -89,7 +22,7 @@ server.get("/", (req, res, next) => {
 
 //S46
 server.get("/:id", (req, res, next) => {
-  Order.findByPk(req.params.id)
+  Order.findByPk(req.params.id, {include: OrderLine})
     .then((orders) => {
       res.json(orders);
     })
