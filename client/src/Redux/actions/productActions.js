0f2866ae99bb1446ede068626207d7906e84e903
Elena B. Gonzalez <elebeatrizgonzalez@gmail.com>
2020-08-20 23:06:11 -0300

    Agrego acciones para editar y agregar un producto

--- client/src/Redux/actions/productActions.js
+++ client/src/Redux/actions/productActions.js
@@ -1,8 +1,8 @@
-import {GET_PRODUCTS, SET_DETAILS, DELETE_PRODUCT} from './constants';
+import { GET_PRODUCTS, SET_DETAILS, DELETE_PRODUCT, EDIT_PRODUCT } from './constants';
 
 //------------  PRODUCTOS BUSCADOS  -----------------------------------------------------------
 
-export function getSearchedProducts(keyword) { 
+export function getSearchedProducts(keyword) {
     return function(dispatch) {
         return fetch(`http://localhost:3001/search?query=${keyword}`)
         .then((r) => r.json())
@@ -18,7 +18,7 @@ export function getSearchedProducts(keyword) {
 
 //------------  PRODUCTOS DE X CATEGORIA  -----------------------------------------------------------
 
-export function getCategoryProducts(category) { 
+export function getCategoryProducts(category) {
     return function(dispatch) {
         return fetch(`http://localhost:3001/categories/${category}`)
         .then(r => r.json())
@@ -48,7 +48,7 @@ export function toProductDetails(id) {
 
 //------------ TODOS LOS PRODUCTOS  -----------------------------------------------------------
 
-export function getAllProducts() { 
+export function getAllProducts() {
     return function(dispatch) {
         return fetch(`http://localhost:3001/products`)
         .then((r) => r.json())
@@ -57,7 +57,7 @@ export function getAllProducts() {
             dispatch({type: GET_PRODUCTS, payload: data})
         })
         .catch((error) => {console.log(error)})
-    } 
+    }
 }
 
 //------------ ELIMINAR UN PRODUCTO --------------------------------------------------
@@ -80,4 +80,54 @@ export function onDeleteProduct(ProductId) {
     }
 }
 
+//---------------EDITAR UN PRODUCTO---------------------------------------------------------
+    export function editProduct(product){
+        console.info('productActions product:', product)
+        return function(dispatch) {
+            return fetch(`http://localhost:3001/products/${product.id}`, {
+                method: 'PUT',
+                body: JSON.stringify(product),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+                console.info(res)
+                dispatch({type: EDIT_PRODUCT, payload: res})
+                alert("El Producto se Editó correctamente")
+                window.location = "/crud";
+            }).catch(err => console.error(err))
+        }
+    }
 
+    //----------------------------
+    export function getProduct(id) {
+        return function(dispatch) {
+            return fetch(`http://localhost:3001/products/${id}`)
+            .then((r) => r.json())
+            .then((data) => {
+                console.log('getProduct:', data);
+                dispatch({type: GET_PRODUCTS, payload: data})
+            })
+            .catch((error) => {
+                alert(error);
+            });
+        }
+    }
+
+//------------------Creat Producto
+export function addProduct(product){
+    const url = 'http://localhost:3001/products/';
+    return function(dispatch)
+    {
+        return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(product),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            console.info(res)
+            dispatch({type: GET_PRODUCTS, payload: res})
+        }).catch(err => console.error(err))
+    }
+}
