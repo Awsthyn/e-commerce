21a4f13454a3a624907e251ceb58c9eb311c2c8f
Elena B. Gonzalez <elebeatrizgonzalez@gmail.com>
2020-08-22 11:30:40 -0300

    Agrego nueva ruta a Order Table y lo muevo a la carpeta orderTable dentro de adminOnly

---
+++ client/src/components/adminOnly/orderTable/OrderTable.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+import { getAllOrders , editOrder } from "../../../Redux/actions/orderActions"
+
+class OrderTable extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.getAllOrders();
+    }
+
+    handleChange(e) {
+        const orderId = Number.parseInt(e.target.getAttribute('data-order-id'))
+        const order = this.props.orders.find(o => o.id === orderId)
+        order.orderStatus = e.target.value
+
+        this.props.editOrder(order)
+        .then(() => alert('El estado de la orden fue modificado.'))
+        .catch(err => alert(`Error: ${err}`))
+    }
+
+    render () {
+        const estadosOptions = ["carrito", "creada", "procesando", "cancelada", "completa"]
+        return (
+
+            <div className="container mt-4">
+                <h2 className="col-11 text-center">Lista de Ordenes</h2>
+                <table className="table">
+                    <thead className="text-center">
+                    <tr>
+                        <th>Nro de Orden</th>
+                        <th>Status</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Email</th>
+                        <th>Total</th>
+                        <th>Editar Estado</th>
+                    </tr>
+                </thead>
+            <tbody>
+            {console.log(this.props.orders)}
+            {this.props.orders.map(order =>(
+                <tr key={order.id}>
+                    <td>{order.id}</td>
+                    <td>{order.orderStatus}</td>
+                    <td>{order.user.first_name}</td>
+                    <td>{order.user.last_name}</td>
+                    <td>{order.user.email}</td>
+                    <td>$ {order.total || order.orderLines.map(e => e.quantity * e.product.price).reduce((a, b) => a + b)}</td>
+                    <td>
+                    <select name='orderStatus' data-order-id={order.id} value={order.orderStatus.toLowerCase()} onChange={this.handleChange}>
+                        {
+                            estadosOptions.map((o) => (
+                                <option key={o} value={o}>{o}</option>
+                            ))
+                        }
+                    </select>
+                    </td>
+                </tr>)
+            )}
+            </tbody>
+            </table>
+            </div>
+    )}
+}
+
+
+function mapStateToProps(state) {
+  return {
+      orders: state.orders.orders,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getAllOrders: () => dispatch(getAllOrders()),
+    editOrder: (order) => dispatch(editOrder(order)),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderTable);
--- client/src/routing/App.js
+++ client/src/routing/App.js
@@ -11,7 +11,7 @@ import CrudProduct from "../components/adminOnly/productCrud/CrudProdut";
 import CrudCategory from "../components/adminOnly/categoryCrud/CrudCategory";
 import Home from "../components/Home.jsx";
 import CrudUser from "../components/adminOnly/userCrud/CrudUser";
-import OrderTable from "../components/OrderTable.jsx";
+import OrderTable from "../components/adminOnly/orderTable/OrderTable.jsx";
 import Order from "../components/Order.jsx";
 import ShoppingCart from "../components/ShoppingCart.jsx";
 import AdminLogin from "../components/adminOnly/adminCruds/AdminLogin";
@@ -45,6 +45,10 @@ function App() {
         exact path="/Admin/CrudAdmin"
         component= { CrudAdmin }
       />
+      <Route
+       exact path = "/Admin/OrderTable"
+       component= { OrderTable }
+     />
 {/* -------------- CRUDS PRODUCT ------------------ */}
       <Route
         exact path="/Admin/CrudProduct"
@@ -88,7 +92,7 @@ function App() {
       <Route
         exact path = "/Admin/CrudUser/:id/edit"
         render = { props =>
-            <EditUser user={props.location.state.user} /> } 
+            <EditUser user={props.location.state.user} /> }
       />
 {/* -------------- ORDEN Y CARRITO ------------------ */}
       <Route
