ab1e309cbff95ec05da9c215bfc70c4f917fbe1d
Elena B. Gonzalez <elebeatrizgonzalez@gmail.com>
2020-08-23 13:18:48 -0300

    Se convierte CrudUsers a componente de clase. Ahora funciona el Delete user.

--- client/src/components/adminOnly/userCrud/CrudUser.jsx
+++ client/src/components/adminOnly/userCrud/CrudUser.jsx
@@ -1,40 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 import { getAllUsers, deleteUser } from "../../../Redux/actions/userActions"
-import { Link } from 'react-router-dom';
-import store from "../../../Redux/store"
 
 
-store.dispatch(getAllUsers());
-export const CrudUser = ({ users }) => {
+class CrudUser extends React.Component{
 
-    return (
-        <div className="container mt-4">
-            <Link to="CrudUser/form/new" className="btn btn-success">Nuevo</Link>
-            <h2 className="col-11 text-center">Edición de usuarios</h2>
-            <table className="table">
-                <thead className="text-center">
-                    <tr>
-                        <th>Usuario</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody  className="text-center">
-                {users.map((e, i) => (
-                    <tr key={e.id}>
-                        <td>{`${e.first_name} ${e.last_name}`}</td>
-                        <td><Link
-                                to={{ pathname: `/Admin/CrudUser/${e.id}/edit`,
-                                    state: { user : e }}}
-                                className= "btn btn-success">Editar</Link></td>
-                        <td><button onClick={deleteUser(e.id)} className="btn btn-danger">Eliminar</button></td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>)
+    componentDidMount(){
+        this.props.getAllUsers()
+    }
+
+    render () {
+
+        return (
+            <div className="container mt-4">
+                <Link to="CrudUser/form/new" className="btn btn-success">Nuevo</Link>
+                <h2 className="col-11 text-center">Edición de usuarios</h2>
+                <table className="table">
+                    <thead className="text-center">
+                        <tr>
+                            <th>Usuario</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody  className="text-center">
+                    {this.props.users.map((e, i) => (
+                        <tr key={e.id}>
+                            <td>{`${e.first_name} ${e.last_name}`}</td>
+                            <td><Link
+                                    to={{ pathname: `/Admin/CrudUser/${e.id}/edit`,
+                                        state: { user : e }}}
+                                    className= "btn btn-success">Editar</Link></td>
+                            <td><button onClick={() => this.props.deleteUser(e.id)} className="btn btn-danger">Eliminar</button></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
+
 function mapStateToProps(state) {
     return {
         users: state.users.users,
@@ -43,7 +51,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-      getAllUsers: () => dispatch(getAllUsers())
+      getAllUsers: () => dispatch(getAllUsers()),
+      deleteUser: (id) => dispatch(deleteUser(id))
     };
 }
 
