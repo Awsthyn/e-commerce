c3b63e4d95fc19907f08903309d2a4fe70c43024
Elena B. Gonzalez <elebeatrizgonzalez@gmail.com>
2020-08-24 19:09:36 -0300

    Quito $0 cuando un producto no es encontrado

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -22,7 +22,7 @@ export function ProductCard({ id, name, price, image, stock, toProductDetails, a
       <img src={image} className={styles.product_card} alt={name} />
       <div className="card-body d-flex flex-column justify-content-center">
   {stock<1 ?<h5 className="card-title text-center"><sup className="bg-danger text-white mr-2 pl-2 pr-2 rounded">Sin Stock</sup>{name}</h5>:<h5 className="card-title text-center">{name}</h5>}
-        <p className="card-text text-center">$ {price}</p>
+        { !price ? <p className="card-text text-center"></p> : <p className="card-text text-center">${price}</p> }
         {id !== -1 ?
         <div className="row">
         <button data-id={id} type= 'button' className="btn btn-dark ml-auto mr-auto" onClick={(e) => {
