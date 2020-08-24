c2e84ce4619ab30c022d239501fbd569c9d0b64d
Awsthyn <aguswagner008@gmail.com>
2020-08-24 08:38:10 -0300

    product y productcard pueden renderizar imagenes de cualquier aspect ratio. Falta editar formularios

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import { toProductDetails } from "../Redux/actions/productActions"
 import { addToOrder } from "../Redux/actions/cartActions"
 import { connect } from "react-redux";
-import store from "../Redux/store"
+import styles from '../css/product.module.css'
 
 
 export function ProductCard({ id, name, price, image, stock, toProductDetails, addToOrder }) {
@@ -19,7 +19,7 @@ export function ProductCard({ id, name, price, image, stock, toProductDetails, a
 
   return(
     <div className="card bg-light p-2 m-3 shadow p-3 mb-5 bg-white rounded" style={{ width: "18rem" }}>
-      <img src={image} className="card-img-top rounded border border-dark" alt={name} />
+      <img src={image} className={styles.product_card} alt={name} />
       <div className="card-body d-flex flex-column justify-content-center">
   {stock<1 ?<h5 className="card-title text-center"><sup className="bg-danger text-white mr-2 pl-2 pr-2 rounded">Sin Stock</sup>{name}</h5>:<h5 className="card-title text-center">{name}</h5>}
         <p className="card-text text-center">$ {price}</p>
