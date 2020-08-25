a3e0b7f08390a393bd6abae0adc706f03ad7bdc6
nico-slk <nico1991.6@gmail.com>
2020-08-24 21:10:38 -0300

    Alerta de compra, vaciado de carrito y agregado al carrito

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getCart, emptyCart, deleteProductFromCart, confirmCart } from '../Redux/actions/cartActions';
 import OrderLine from '../components/OrderLine';
+import swal from 'sweetalert';
+
+const confirmar = (tit, tex, tim, suc, func) => {
+	swal({
+		title: tit, //"¿Finalizar compra?",
+		text: tex, //"¿Desea completar la compra de los productos del carrito?",
+		icon: "warning",
+		buttons: ["No", "Si"],
+		dangerMode: true,
+	})
+		.then((willBuy) => {
+			if (willBuy) {
+				swal({
+					text: suc,
+					icon: "success",
+					timer: tim, //"4000"
+				});
+			}
+			if (func && willBuy) {
+				func()
+				console.log("ACEPTADO")
+			} else {
+				console.log("CANELADO")
+			}
+		});
+}
 
 class Order extends Component {
 
-    componentDidMount() {
-        this.props.getCart();
-    }
-    
+	componentDidMount() {
+		this.props.getCart();
+	}
+
+
 	render() {
-	const {cart} = this.props;
-		return ( 
+		const { cart } = this.props;
+		return (
 			<div>
 				<h1 className="d-flex justify-content-center m-3">Carrito</h1>
 				<div className="">
@@ -40,24 +67,25 @@ class Order extends Component {
 						</tbody>
 					</table>
 					<div className="mt-4 d-flex justify-content-around">
-						<h5 id="total" className="border border-success p-3" onClick={()=>console.log(document.getElementById("total").innerHTML.slice(8))}>
+						<h5 id="total" className="border border-success p-3" onClick={() => console.log(document.getElementById("total").innerHTML.slice(8))}>
 							Total: $
 							{cart[0] ? cart.map((e) => e.quantity * e.product.price).reduce((a, b) => a + b) : 0}
 						</h5>
 						<div>
 							<button className="btn btn-success" onClick={() => {
 								this.props.confirmCart(document.getElementById("total").innerHTML.slice(8))
-								alert("La compra se ha procesado exitosamente. En breve nos pondremos en contacto con usted")
-								window.location = "/"
+								confirmar("¿Finalizar compra?", "¿Desea completar la compra de los productos del carrito?", "4000", "Su compra ha sido finalizada", this.props.emptyCart)
 							}
-								}>Confirmar compra</button>
-							<button className="btn btn-danger" onClick={this.props.emptyCart}>
+							}>Confirmar compra</button>
+							<button className="btn btn-danger" onClick={() => {
+								confirmar("¿Vaciar carrito?", "¿Desea eliminar todos productos del carrito?", "4000", "Su compra ha sido vaciado", this.props.emptyCart)
+							}}>
 								Vaciar carrito
 							</button>
 						</div>
 					</div>
 				</div>
-              </div>
+			</div>
 		);
 	}
 }
@@ -70,10 +98,10 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
 	return {
-    getCart: () => dispatch(getCart()),
-	emptyCart: () => dispatch(emptyCart()),
-	deleteProductFromCart: (id) => dispatch(deleteProductFromCart(id)),
-	confirmCart: (total) => dispatch(confirmCart(total))
+		getCart: () => dispatch(getCart()),
+		emptyCart: () => dispatch(emptyCart()),
+		deleteProductFromCart: (id) => dispatch(deleteProductFromCart(id)),
+		confirmCart: (total) => dispatch(confirmCart(total))
 	};
 }
 
