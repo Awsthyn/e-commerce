a1a8d87d3baa16deabef7512fa9275ee63cf6126
nico-slk <nico1991.6@gmail.com>
2020-08-27 14:35:14 -0300

    Ventana modal + cursorPointer en logo

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -17,33 +17,35 @@ const alerta = (tit, tex, tim) => {
 
 export function ProductCard({ sessionUser, id, name, price, image, stock, toProductDetails, addToOrder, getCart, cart, editQuantity }) {
   let history = useHistory()
-  useEffect(()=>{
+  useEffect(() => {
     getCart(sessionUser.id)
   }, [])
 
   function handleCart(id) {
     let indexProductCart = cart.findIndex(e => e.product.id === id)
-    if(indexProductCart === -1) {
-      if(stock < 1) {swal("Lo sentimos", "No se ha podido agregar a carrito debido a falta temporal de stock.", "error")}
+    if (indexProductCart === -1) {
+      if (stock < 1) { swal("Lo sentimos", "No se ha podido agregar a carrito debido a falta temporal de stock.", "error") }
       else {
-        addToOrder(id, 1, sessionUser.id); 
-        alerta("Agregado", "El producto se agregó al carrito correctamente", "4000")}
-    }
-    else {
-      if(stock <= cart[indexProductCart].quantity) {swal("Lo sentimos", "no disponemos de la cantidad que usted está solicitando", "error")}
-      else {
-        editQuantity(cart[indexProductCart].id, cart[indexProductCart].quantity + 1, sessionUser.id)
-        alerta("Agregado", "El producto se agregó al carrito correctamente", "4000")}
-        getCart(sessionUser.id)
+        addToOrder(id, 1, sessionUser.id);
+        alerta("Agregado", "El producto se agregó al carrito correctamente", "4000")
       }
     }
+    else {
+      if (stock <= cart[indexProductCart].quantity) { swal("Lo sentimos", "no disponemos de la cantidad que usted está solicitando", "error") }
+      else {
+        editQuantity(cart[indexProductCart].id, cart[indexProductCart].quantity + 1, sessionUser.id)
+        alerta("Agregado", "El producto se agregó al carrito correctamente", "4000")
+      }
+      getCart(sessionUser.id)
+    }
+  }
 
   return (
     <div className="card bg-light p-2 m-3 shadow p-3 mb-5 bg-white rounded" style={{ width: "18rem" }}>
       <img src={image} className={styles.product_card} alt={name} />
       <div className="card-body d-flex flex-column justify-content-center">
-  {stock<1 ?<h5 className="card-title text-center"><sup className="bg-danger text-white mr-2 pl-2 pr-2 rounded">Sin Stock</sup>{name}</h5>:<h5 className="card-title text-center">{name}</h5>}
-        { !price ? <p className="card-text text-center"></p> : <p className="card-text text-center">${price}</p> }
+        {stock < 1 ? <h5 className="card-title text-center"><sup className="bg-danger text-white mr-2 pl-2 pr-2 rounded">Sin Stock</sup>{name}</h5> : <h5 className="card-title text-center">{name}</h5>}
+        {!price ? <p className="card-text text-center"></p> : <p className="card-text text-center">${price}</p>}
         {id !== -1 ?
           <div className="row">
             <button data-id={id} type='button' className="btn btn-dark ml-auto mr-auto" onClick={(e) => {
