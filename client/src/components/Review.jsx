b6363293088855ed18e3ce2a4c746079280ba769
Awsthyn <aguswagner008@gmail.com>
2020-08-31 09:27:51 -0300

    corregido para q no se muestre a guest

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -80,7 +80,7 @@ export class Review extends React.Component {
 		if(this.props.productDetails.reviews) indexReview = Number(this.props.productDetails.reviews.findIndex((e)=> Number(e.userId)=== Number(this.props.sessionUser.id)))
 		return (
 			<div>
-				{indexReview === -1 ? (<div>
+				{indexReview === -1 && this.props.sessionUser.id ? (<div>
 					<form onSubmit={this.handleSubmit} className="form-group">
 						<RatingThumbs clickHandler={this.handleRating} />
 						<label>Escriba su comentario. </label>
